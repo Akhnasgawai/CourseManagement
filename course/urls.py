a85e37a51fd5552b4bda22432bfae7e301798c4e
Mohammed Akhnas Gawai <akhnasg@gmail.com>
2023-08-01 11:49:49 +0530

    base route changed

--- course/urls.py
+++ course/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
+    path("", views.login_view, name="login"),
     path(
         "verify_otp/<str:username>/<str:role>/",
         views.verify_otp_view,
